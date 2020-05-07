69446cd93378865faaf54157d9c1e85b01bf2772
Berar Emanuel <berar.emanuel21@gmail.com>
2020-05-07 09:21:11 +0300

    Setup + Firebase & Facebook config

--- App.js
+++ App.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Facebook from 'expo-facebook';
+import * as firebase from 'firebase';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { config, FACEBOOK_APP_ID } from './config/firebase';
+
+Facebook.initializeAsync(`${config.appId}`, 'firebase-login-starter');
+firebase.initializeApp(config);
+
+const auth = firebase.auth();
 
 export default function App() {
+  const [loginStatus, setLoginStatus] = useState('Signed out');
+  const [errorMessage, setErrorMessage] = useState('None');
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user != null) {
+        setLoginStatus('We are authenticated now');
+      } else {
+        setLoginStatus('You are currently logged out.');
+      }
+    });
+  }, []);
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp({});
+  }
+
+  const handleFacebookButton = async () => {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
+      permissions: ['public_profile', 'email'],
+    });
+    console.log(type, 'type');
+    if (type === 'success') {
+      //Firebase credential is created with the Facebook access token.
+      const credential = firebase.auth.FacebookAuthProvider.credential(token);
+      auth.signInWithCredential(credential).catch((error) => {
+        setErrorMessage(error.message);
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableHighlight
+        style={styles.facebookButton}
+        name="Facebook"
+        underlayColor={styles.facebookButton.backgroundColor}
+        onPress={() => handleFacebookButton()}
+      >
+        <Text style={styles.facebookButtonText}>Log in with Facebook</Text>
+      </TouchableHighlight>
+      <View style={styles.space} />
+      <Text>Logged In Status: {loginStatus}</Text>
+
+      <View style={styles.space} />
+      <Text> Log In Error Messages: {errorMessage}</Text>
     </View>
   );
 }
@@ -15,5 +66,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  facebookButton: {
+    width: 375 * 0.75,
+    height: 48,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3B5998',
+  },
+  facebookButtonText: {
+    color: '#fff',
+  },
+  space: {
+    height: 17,
   },
 });
---
+++ config/firebase.js
@@ -0,0 +1,12 @@
+export const FACEBOOK_APP_ID = '1174282509639120';
+
+export const config = {
+  apiKey: 'AIzaSyATDrQrxVPVVaOqeyi9AYGC9QwoEgWZBH4',
+  authDomain: 'fir-login-starter.firebaseapp.com',
+  databaseURL: 'https://fir-login-starter.firebaseio.com',
+  projectId: 'fir-login-starter',
+  storageBucket: 'fir-login-starter.appspot.com',
+  messagingSenderId: '362129773005',
+  appId: '1:362129773005:web:872b7cfaeccf4494513484',
+  measurementId: 'G-0DZDK6049G',
+};
